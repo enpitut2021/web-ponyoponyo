12fd6bf43796a53250b7a36adb81f1e0bd78d14b
Kaya815 <66908665+Kaya815@users.noreply.github.com>
2021-12-01 12:50:10 +0900

    Add files via upload
    episode_page renew

--- pages/edit/episode.tsx
+++ pages/edit/episode.tsx
@@ -1,35 +1,34 @@
-import React, { useRef } from "react";
-import axios from "axios";
+import React ,{ useRef } from 'react';
+import axios from 'axios';
+import styles from "./episode.module.css";
+import imgURL from './1.png';
+
 
 const episode_page = () => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const inputEl1 = useRef<HTMLInputElement>(null);
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const inputEl2 = useRef<HTMLInputElement>(null);
-  const handleClick = () => {
-    axios({
-      method: "POST",
-      url: "https://api.digital-future.jp/episode",
-      data: {
-        user_id: inputEl1.current!!.value,
-        desc: inputEl2.current!!.value,
-      },
-    }).then((response) => console.log("response body:", response.data));
-  };
+    const inputEl1 = useRef<HTMLDivElement>(null);
+    const inputEl2 = useRef<HTMLDivElement>(null);
+    const handleClick = () => { 
+        axios({
+            method : 'POST',
+            url    : 'http://localhost:9002/episode',
+            data   : { user_id: inputEl1.current.value, desc: inputEl2.current.value}
+        }).then(response => console.log('response body:', response.data));
+    };
 
-  return (
-    <>
-      <div>
-        <input
-          ref={inputEl1}
-          type="text"
-          placeholder={"create user name here"}
-        />
-        <br />
-        <input ref={inputEl2} type="text" placeholder={"input espisode here"} />
-        <button onClick={handleClick}>登録</button>
-      </div>
-    </>
-  );
-};
+    return (
+        
+        <div className={styles.input}>
+            <h1 className ={styles.title}>恥</h1>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <input className ={styles.font_2} ref={inputEl1} type="text" placeholder={"ユーザー名を入力"}/>
+            <br/>
+            <input className ={styles.font_2} ref={inputEl2} type="text" placeholder={"恥ずかしエピソードを入力"}/>
+            <br/>
+            <button className ={styles.font}onClick={handleClick}>登録</button>
+        </div>
+    )
+}
 export default episode_page;
