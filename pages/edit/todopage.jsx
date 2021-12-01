7ef6d977a3b8f3a74492dffb0789105cab162b24
notch_man <30828280+notchman8600@users.noreply.github.com>
2021-12-01 14:47:04 +0900

    Update todopage.jsx

--- pages/edit/todopage.jsx
+++ pages/edit/todopage.jsx
@@ -2,9 +2,9 @@ import React, {useRef, useEffect, useState} from 'react';
 import axios from 'axios';
 
 const todo_page = () => {
-    const inputEl1 = useRef < HTMLInputElement > (null);
-    const inputEl2 = useRef < HTMLInputElement > (null);
-    const inputEl3 = useRef < HTMLInputElement > (null);
+    const inputEl1 = useRef(null);
+    const inputEl2 = useRef(null);
+    const inputEl3 = useRef(null);
 
     const handleClick = () => {
         const cur_time = new Date()
@@ -13,7 +13,7 @@ const todo_page = () => {
         if (inputEl1.current.value.length !== 0 && getTime < getDdl) {
             axios({
                 method: 'POST',
-                url: 'https://api.digital-future.jp/task/read',
+                url: 'https://api.digital-future.jp/task',
                 data: {name: inputEl1.current.value, deadline: inputEl2.current.value, is_finished: false}
             }).then(response => console.log('response body:', response.data));
         }
@@ -22,14 +22,12 @@ const todo_page = () => {
     // const data = [{name: "finish my work", deadline: "2021-12-05", id: "1", is_finished: true}]
 
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [data, setData] = useState();
+    const [data, setData] = useState([]);
 
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useEffect(() => {
         const fetchData = async () => {
-            const result = await axios(
-                'https://api.digital-future.jp/task',
-            );
+            const result = await axios('https://api.digital-future.jp/task/read',);
 
             setData(result.data);
         };
@@ -41,15 +39,14 @@ const todo_page = () => {
     const handleOnchange = () => {
         axios({
             method: 'POST',
-            url: 'https://api.digital-future.jp/task/read',
+            url: 'https://api.digital-future.jp/task/',
             data: {id: inputEl3.current.id, is_finished: inputEl3.current.checked}
         }).then(response => console.log('response body:', response.data));
     }
 
 
     // @ts-ignore
-    return (
-        <>
+    return (<>
             <div>
                 <input
                     ref={inputEl1}
@@ -64,9 +61,7 @@ const todo_page = () => {
                     onClick={handleClick}
                 >登録
                 </button>
-
-                data !== undefined ?
-                {data.map(item => (
+                {data !== undefined ? data.map(item => (
 
                     <ul key={uuid.v4()}>
                         <li>
@@ -84,10 +79,8 @@ const todo_page = () => {
                                 {item.name}&emsp;&emsp;&emsp;{item.deadline}
                             </label>
                         </li>
-                    </ul>
-                ))}:{}
+                    </ul>)) : <div></div>}
             </div>
-        </>
-    )
+        </>)
 }
 export default todo_page;
